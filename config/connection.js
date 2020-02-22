8930d5fa684029c05a6c272128780cb53b046c01
Alex Happe <abhdiver1994@gmail.com>
2020-02-22 10:32:46 -0600

    trying to figure out what this wants from me

--- config/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const Workout = require("../models/fitnessModels");
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbWorkout", {
-  useNewUrlParser: true
-});
-
-module.exports = connection;
--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express");
-const logger = require("morgan");
-const PORT = process.env.PORT || 3000;
-const db = require("./models/fitnessModels");
-const app = express();
+const mongoose = require("mongoose");
 
-app.use(logger("dev"));
+const PORT = process.env.PORT || 3000;
+
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
